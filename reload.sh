5b6cf84dbc242f63ad9f9fdf133444ec7b9e0f9d
Jarek Lipski <loomchild@loomchild.net>
2015-02-20 13:42:57 +0100

    supoort for .reloadignore file

--- reload.sh
+++ reload.sh
@@ -10,11 +10,31 @@ function ctrlc() {
 
 trap ctrlc INT
 
+if [ $# -eq 0 ]; then
+	echo Specify command to run
+	exit 1
+fi
+
+IGNORE_FILE=.reloadignore
+IGNORE=""
+
+if [ -f $IGNORE_FILE ]; then
+	IGNORE=`echo $(cat .reloadignore) | tr " " "|"`
+fi
+
+echo Watching current directory, ignored file pattern: $IGNORE
+
+INOTIFY_OPTS="-r -q -e modify"
+
+if [ -n "$IGNORE" ]; then
+	INOTIFY_OPTS="$INOTIFY_OPTS --exclude \"$IGNORE\""
+fi
 
 while true; do
   $@ &
   PID=$!
-  inotifywait -r -q -e modify --exclude "\.(pyc|sw?|log)" .
+  inotifywait $INOTIFY_OPTS .
   disown $PID
   kill $PID
 done
+
