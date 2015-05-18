9bf1658da57ec1be51a04c165c25625b27f55b95
Jarek Lipski <loomchild@loomchild.net>
2015-05-18 23:40:46 +0200

    bump version to 3.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "reload",
-    version = "0.3",
+    version = "0.4",
     author = "Jarek Lipski",
     author_email = "pub@loomchild.net",
     description = ("Reload a program if any file in current directory changes."),
